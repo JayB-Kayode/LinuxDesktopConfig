4d97980af92e4830226de45589ff36299473b811
jean-brice <jdickewu@gmail.com>
2017-09-13 10:27:48 +0000

    added code to install gnome simple dock shell

--- setupdesktoplinux.sh
+++ setupdesktoplinux.sh
@@ -5,3 +5,11 @@
 
 sudo dnf install xenlism-wildfire-0-0.2.20160511gitd3b9ad2.fc26.noarch 
 
+#gnome shell simple dock 
+
+cd ~
+git clone https://github.com/optimisme/gnome-shell-simple-dock.git
+cd gnome-shell-simple-dock
+chmod 744 install.sh
+./install.sh
+
